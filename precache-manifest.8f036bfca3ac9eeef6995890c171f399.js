67ed77cdfebb4cfd5bc85fd6f5dc4b69d392986f
tamara96mc <tamara.96mc@gmail.com>
2020-12-09 20:15:30 +0100

    Updates

---
+++ precache-manifest.8f036bfca3ac9eeef6995890c171f399.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d2599c37ca8a7ba32bd422fad358a5f",
+    "url": "/monedas/index.html"
+  },
+  {
+    "revision": "8328ce155b3249d84a27",
+    "url": "/monedas/static/css/main.59fadb45.chunk.css"
+  },
+  {
+    "revision": "88839fb5aac0a123127b",
+    "url": "/monedas/static/js/2.146fad28.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/monedas/static/js/2.146fad28.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8328ce155b3249d84a27",
+    "url": "/monedas/static/js/main.f4f378ff.chunk.js"
+  },
+  {
+    "revision": "f35d17fa2dca2bb8e267",
+    "url": "/monedas/static/js/runtime-main.5f4a2a7f.js"
+  },
+  {
+    "revision": "96e45d694833f161c9dc02ab9dde4a6b",
+    "url": "/monedas/static/media/cryptomonedas.96e45d69.png"
+  }
+]);
